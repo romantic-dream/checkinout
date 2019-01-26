e869046102d8dd7db6b080bc58bbf2086b1bf178
romantic-dream <921808986@qq.com>
2019-01-26 21:03:06 +0800

    check in out

--- src/main/java/com/qiu/mobileoa/checkinout/CheckinoutApplication.java
+++ src/main/java/com/qiu/mobileoa/checkinout/CheckinoutApplication.java
@@ -1,9 +1,11 @@
 package com.qiu.mobileoa.checkinout;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.qiu.mobileoa.checkinout.dao")
 public class CheckinoutApplication {
 
     public static void main(String[] args) {
