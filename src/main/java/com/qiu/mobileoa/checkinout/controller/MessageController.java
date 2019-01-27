340d4ea2cc7a88dfc407aba9616c8e413496b45a
romantic-dream <921808986@qq.com>
2019-01-27 14:51:25 +0800

    check in out

--- src/main/java/com/qiu/mobileoa/checkinout/controller/MessageController.java
+++ src/main/java/com/qiu/mobileoa/checkinout/controller/MessageController.java
@@ -3,10 +3,9 @@ package com.qiu.mobileoa.checkinout.controller;
 import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 
 @RestController
@@ -15,8 +14,10 @@ public class MessageController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @PostMapping("/receive")
-    public void receive(@RequestBody JSONObject object){
-        logger.info("{}",object);
+    @GetMapping("/receive")
+    public String receive(@RequestParam Map<String,String> allparams){
+        logger.info("{}",allparams);
+        String echostr = allparams.get("echostr");
+        return echostr;
     }
 }
