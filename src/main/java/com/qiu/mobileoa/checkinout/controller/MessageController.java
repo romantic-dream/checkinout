ca4bc325c478778b6b546fb22e6461d711f92c8f
romantic-dream <921808986@qq.com>
2019-01-27 16:04:34 +0800

    jackson 使用注解转换

--- src/main/java/com/qiu/mobileoa/checkinout/controller/MessageController.java
+++ src/main/java/com/qiu/mobileoa/checkinout/controller/MessageController.java
@@ -1,12 +1,13 @@
 package com.qiu.mobileoa.checkinout.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.qiu.mobileoa.checkinout.dto.MessageAutoResponseDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
-
+import java.util.Date;
 
 @RestController
 @RequestMapping("/message")
@@ -14,10 +15,22 @@ public class MessageController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @GetMapping("/receive")
+    /*@GetMapping("/receive")
     public String receive(@RequestParam Map<String,String> allparams){
         logger.info("{}",allparams);
         String echostr = allparams.get("echostr");
         return echostr;
+    }*/
+
+    @PostMapping(value = "receive",produces = MediaType.APPLICATION_XML_VALUE)
+    public MessageAutoResponseDTO receive(@RequestBody JSONObject jsonObject){
+        logger.info("{}",jsonObject);
+        MessageAutoResponseDTO messageAutoResponseDTO = new MessageAutoResponseDTO();
+        messageAutoResponseDTO.setToUserName("oo7si1eVBb6OYP-zGINj5yspYv7E");
+        messageAutoResponseDTO.setFromUserName("gh_242a22109582");
+        messageAutoResponseDTO.setCreateTime(new Date().getTime());
+        messageAutoResponseDTO.setMsgType("text");
+        messageAutoResponseDTO.setContent("welcome");
+        return messageAutoResponseDTO;
     }
 }
