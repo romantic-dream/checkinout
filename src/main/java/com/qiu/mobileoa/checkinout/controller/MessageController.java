c7af6522533a09f18d54906150ebcf711e7cb1dd
romantic-dream <921808986@qq.com>
2019-01-27 20:09:23 +0800

    jackson 使用注解转换

--- src/main/java/com/qiu/mobileoa/checkinout/controller/MessageController.java
+++ src/main/java/com/qiu/mobileoa/checkinout/controller/MessageController.java
@@ -2,11 +2,17 @@ package com.qiu.mobileoa.checkinout.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.qiu.mobileoa.checkinout.dto.MessageAutoResponseDTO;
+import com.qiu.mobileoa.checkinout.po.User;
+import com.qiu.mobileoa.checkinout.service.impl.UserServiceImpl;
+import com.qiu.mobileoa.checkinout.service.impl.WeixinClientImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.Date;
 
 @RestController
@@ -14,6 +20,15 @@ import java.util.Date;
 public class MessageController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private WeixinClientImpl weixinClient;
+
+    @Autowired
+    private UserServiceImpl userService;
+
+    @Value("${weixin.accessToken}")
+    private String accessToken;
 
     /*@GetMapping("/receive")
     public String receive(@RequestParam Map<String,String> allparams){
@@ -23,14 +38,36 @@ public class MessageController {
     }*/
 
     @PostMapping(value = "receive",produces = MediaType.APPLICATION_XML_VALUE)
-    public MessageAutoResponseDTO receive(@RequestBody JSONObject jsonObject){
+    public Object receive(@RequestBody JSONObject jsonObject) throws IOException {
         logger.info("{}",jsonObject);
-        MessageAutoResponseDTO messageAutoResponseDTO = new MessageAutoResponseDTO();
-        messageAutoResponseDTO.setToUserName("oo7si1eVBb6OYP-zGINj5yspYv7E");
-        messageAutoResponseDTO.setFromUserName("gh_242a22109582");
-        messageAutoResponseDTO.setCreateTime(new Date().getTime());
-        messageAutoResponseDTO.setMsgType("text");
-        messageAutoResponseDTO.setContent("welcome");
-        return messageAutoResponseDTO;
+        String msgType = jsonObject.getString("MsgType");
+        if (msgType == "event"){
+            String fromUserName = jsonObject.getString("FromUserName");
+            String toUserName = jsonObject.getString("ToUserName");
+            JSONObject userInfo = weixinClient.getSnsUserInfo(accessToken,fromUserName);
+            String nickname = userInfo.getString("nickname");
+            String openid = userInfo.getString("openid");
+
+            MessageAutoResponseDTO messageAutoResponseDTO = new MessageAutoResponseDTO();
+            messageAutoResponseDTO.setToUserName(fromUserName);
+            messageAutoResponseDTO.setFromUserName(toUserName);
+            messageAutoResponseDTO.setCreateTime(new Date().getTime());
+            messageAutoResponseDTO.setMsgType("text");
+            messageAutoResponseDTO.setContent(String.format("你好，%s,欢迎订阅！",nickname));
+
+            User user = new User();
+            user.setOpenid(openid);
+            Integer gender = userInfo.getInteger("sex");
+            String avatarUrl = userInfo.getString("headimgurl");
+            user.setNickname(nickname);
+            user.setGender(gender);
+            user.setAvatarUrl(avatarUrl);
+            userService.create(user);
+            return messageAutoResponseDTO;
+        }
+
+
+
+        return null;
     }
 }
